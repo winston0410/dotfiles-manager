556744af2a4234a122ec03dc94aa9653030bfcd5
Hugo Sum <hugosum@Hugos-MacBook-Pro.local>
2021-09-14 10:23:15 +0100

    Provide options for zoxide

--- options/programs.nix
+++ options/programs.nix
@@ -6,16 +6,18 @@ with lib;
 let
   cfg = config.dotfiles.programs;
   shellCfg = config.dotfiles.terminal.shell;
+  programOptions = programName: {
+    enable = mkEnableOption programName;
+    package = mkOption {
+      type = types.package;
+      description = "Default package for ${programName}";
+      default = pkgs.direnv;
+    };
+  };
 in {
   options.dotfiles.programs = {
-    direnv = {
-      enable = mkEnableOption "direnv";
-      package = mkOption {
-        type = types.package;
-        description = "Default direnv package";
-        default = pkgs.direnv;
-      };
-    };
+    direnv = programOptions "direnv";
+    zoxide = programOptions "zoxide";
   };
 
   config = (mkMerge [
@@ -26,12 +28,31 @@ in {
         };
       }
       (mkIf (shellCfg.package.pname == "zsh") {
-        dotfiles.terminal.shell.init = ''eval "$(direnv hook zsh)"'';
+        dotfiles.terminal.shell.init = ''
+          eval "$(${
+            config.lib.custom.getBinPath cfg.direnv.package
+          } hook zsh)"'';
       })
 
-      (mkIf (shellCfg.package.pname == "nushell") {
-        dotfiles.terminal.shell.init = ''eval "$(direnv hook zsh)"'';
+      # (mkIf (shellCfg.package.pname == "nushell") {
+      # dotfiles.terminal.shell.init = ''eval "$(direnv hook zsh)"'';
+      # })
+    ])
+
+    ((mkIf cfg.zoxide.enable) mkMerge [
+      {
+        home-manager.users.${username} = {
+          home.packages = [ cfg.zoxide.package ];
+        };
+      }
+      (mkIf (shellCfg.package.pname == "zsh") {
+        dotfiles.terminal.shell.init =
+          ''eval "$(${config.lib.custom.getBinPath cfg.zoxide.package} zsh)"'';
       })
+
+      # (mkIf (shellCfg.package.pname == "nushell") {
+      # dotfiles.terminal.shell.init = ''eval "$(direnv hook zsh)"'';
+      # })
     ])
   ]);
 }
