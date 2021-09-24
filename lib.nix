0675ea4401f4d07d1ed63efa31c14ca91bd52931
kghugo <hugosum.dev@protonmail.com>
2021-09-24 08:51:25 +0100

    Create dotfiles-manager modules for improving home-manager

---
+++ lib.nix
@@ -0,0 +1,70 @@
+nixpkgs:
+let
+  # override all datatypes except lists, which will be extended(inheritance) instead
+  # Deprecated at the moment
+  # extendAttrs = oldArgs: newArgs:
+  # let
+  # zipped = nixpkgs.lib.attrsets.zipAttrs [ oldArgs newArgs ];
+
+  # foldFn = value:
+  # (nixpkgs.lib.lists.foldl (acc: cur:
+  # if builtins.isList cur then
+  # acc ++ cur
+  # else
+  # (if (builtins.isAttrs cur) then (extendAttrs acc cur) else cur))
+  # (if (builtins.isList (builtins.elemAt value 0)) then [ ] else { })
+  # value);
+  # mapFn = (_: value: (foldFn value));
+
+  # in (nixpkgs.lib.attrsets.mapAttrs (mapFn) zipped);
+
+  mkExtendable = f: origArgs:
+    let
+      # Only accept function as the result.
+      result = (f origArgs);
+
+      extendAttrs = oldArgs: newArgs:
+        nixpkgs.lib.attrsets.recursiveUpdate oldArgs newArgs;
+
+      # setFunctionArgs returns a set???
+    in (nixpkgs.lib.setFunctionArgs result (nixpkgs.lib.functionArgs result)
+      // {
+        # TODO: Fix error where recursion behavior doesn't work here
+        override = newArgs: (f (extendAttrs origArgs newArgs));
+        # override = newArgs: (mkExtendable (f (extendAttrs origArgs newArgs)));
+      });
+
+  toList = nixpkgs.lib.attrsets.mapAttrsToList (_: value: value);
+
+in {
+  mkProfile = mkExtendable
+    ({ modules ? { }, hmModules ? { }, namedModules ? { }, userProfile ? { } }:
+      # return an NixOS module to prevent leaking extend/override into configuration
+      ({ ... }:
+        let applyName = builtins.map (m: (m userProfile.name));
+        in {
+          imports = (toList modules)
+            ++ (nixpkgs.lib.trivial.pipe namedModules [ toList applyName ]);
+          # create user profile here
+          users.users.${userProfile.name} = userProfile;
+
+          # Handle home-manager modules
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${userProfile.name} = {
+            imports = (toList hmModules);
+          };
+        }));
+
+  mkSystem = mkExtendable ({ system, inputs, modules ? { } }:
+    # TODO: assert no users.users config in modules in mkSystem
+    let
+      hasHm = (builtins.hasAttr "home-manager" inputs)
+        || (builtins.hasAttr "hm" inputs);
+    in (inputs.nixpkgs.lib.nixosSystem {
+      inherit system;
+      modules = (toList modules) ++ (inputs.nixpkgs.lib.optional hasHm
+        inputs.home-manager.nixosModules.home-manager);
+      specialArgs = { inherit inputs system; };
+    }));
+}
--- options.nix
+++ options.nix
@@ -1,35 +1,19 @@
-username:
-{ lib, config, ... }:
+{ lib, config, pkgs, ... }:
 
 with lib;
 
-{
-  imports = (lib.lists.forEach [
-    ./options/terminal.nix
-    ./options/programs.nix
-    ./options/xserver.nix
-    # ./options/network-manager.nix
-  ] (module: (import module) username));
+let tomlFormat = pkgs.formats.toml { };
+in {
+  imports = [ ./options/shellAliases.nix ];
 
-  config.lib.custom = let
-    # Make this function private
-    getBinPath = package:
-      if (builtins.hasAttr "shellPath" package) then
-        package.shellPath
-      else
-        "/bin/${package.pname}";
-    addErrPrefix = str: "Dotfile Manager: " + str;
-  in {
-    addErrPrefix = addErrPrefix;
-    getExecPath = package: "${package}${(getBinPath package)}";
-    hydratePlaceholder = from: to: path:
-      let
-        str = (builtins.readFile path);
-        replaced = (builtins.replaceStrings from to str);
-      in (if (replaced != str) then
-        replaced
-      else
-        (abort (addErrPrefix
-          "Placeholder replacement failed for '${path}'. Have you included the placeholder in your config?")));
+  config = {
+    home.file = {
+      ".zshrc" = { text = config.programs.zsh.initExtra; };
+      ".bashrc" = { text = config.programs.bash.initExtra; };
+    };
+
+    xdg.configFile = {
+      "fish/config.fish" = { text = config.programs.fish.shellInit; };
+    };
   };
 }
